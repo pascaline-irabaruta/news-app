3835c9b5984ade88b848a1c30dec32e7102b619e
pascaline-irabaruta <pascyirabaruta456@gmail.com>
2020-11-30 07:40:01 +0200

    created a function to configure request

---
+++ app/models.py
@@ -0,0 +1,27 @@
+class News_source:
+    '''
+    Class for instantiating Source objects
+    '''
+    def __init__(self,name,description,category,url,id):
+        '''
+        Method for instantiating the movie variables
+        '''
+        self.name = name
+        self.description = description
+        self.category =category
+        self.url = url
+        self.id = id
+class News_article:
+    '''
+    Class for instantiating article objects
+    '''
+
+    def __init__(self,image,description,timecreated,articlelink):
+        '''
+        Method for instantiating article variables
+        '''
+        #self.source = source
+        self.image = image
+        self.description = description
+        self.timecreated = timecreated
+        self.articlelink = articlelink
---
+++ app/requests.py
@@ -0,0 +1,11 @@
+from .models import News_article,News_source
+import urllib.request,json
+
+api_key = None
+
+base_url = None
+
+def configure_request(app):
+    global api_key,base_url
+    api_key = app.config['NEWS_API_KEY']
+    base_url = app.config['NEWS_API_BASE_URL']
