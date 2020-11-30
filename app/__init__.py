f6486888434e9f717c08524736bfc68a91b30595
pascaline-irabaruta <pascyirabaruta456@gmail.com>
2020-11-30 07:33:49 +0200

    created a model classs news article

--- app/__init__.py
+++ app/__init__.py
@@ -11,3 +11,17 @@ class News_source:
         self.category =category
         self.url = url
         self.id = id
+class News_article:
+    '''
+    Class for instantiating article objects
+    '''
+
+    def __init__(self,image,description,timecreated,articlelink):
+        '''
+        Method for instantiating article variables
+        '''
+        #self.source = source
+        self.image = image
+        self.description = description
+        self.timecreated = timecreated
+        self.articlelink = articlelink
